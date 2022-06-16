d2dbbaa19cb981b71b7e25185dd633adfbf5e934
Станислав <stanislav@MacBook-Pro-Stanislav.local>
2022-06-16 19:25:16 +0300

    Create Main View Controller

---
+++ YouTubeMagicTest/View/MainViewController/MainViewController + UITableView.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController + UITableView.swift
+//  YouTubeMagicTest
+//
+//  Created by Станислав on 16.06.2022.
+//
+
+import Foundation
---
+++ YouTubeMagicTest/View/MainViewController/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YouTubeMagicTest
+//
+//  Created by Станислав on 15.06.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
